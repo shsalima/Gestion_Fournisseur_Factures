413c5bb8e99eaf673d402635277a34b63f3ded04
salima <sahisalima77@gmail.com>
2026-04-12 18:58:56 +0200

    travail sur totalAmount de dachbrd

--- controllers/dashbordController.js
+++ controllers/dashbordController.js
@@ -1,13 +1,31 @@
+import mongoose from "mongoose"
 import Facture from "../models/Facture.js"
 import Fournisseur from "../models/Fournisseur.js"
 
 
 export const getDashbordDT=async(req,res)=>{
     try{
-        const userId=req.user.id
+        const userId=new mongoose.Types.ObjectId(req.user.id)
         const totalFournisseurs=await Fournisseur.countDocuments({user:userId})
         const totalInvoices=await Facture.countDocuments({userId:userId})
-        return res.status(200).json({totalFournisseurs, totalInvoices})
+        const totalAmountAgg=await Facture.aggregate([
+            {
+                $match:{userId: userId}
+            },
+            {
+                $group:{
+                    _id:null,
+                    total:{$sum:"$amount"}
+                }
+
+            }
+
+        ])
+          const totalAmount =
+      totalAmountAgg.length > 0
+        ? totalAmountAgg[0].total
+    : 0;
+        return res.status(200).json({totalFournisseurs, totalInvoices, totalAmount})
         
       
     }catch(err){
